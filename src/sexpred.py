e990259ce57b36b8ed169a03717bce140056e90e
Andrés Lou <Andrs Lou>
2019-12-02 12:37:08 -0500

    restructure and touch-up

---
+++ src/sexpred.py
@@ -0,0 +1,89 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class Encoder(nn.Module):
+    def __init__(self,
+                 embeddings_tensor,
+                 hidden_size=40,
+                 dropout=0.5):
+        super(Encoder, self).__init__()
+        self.embedding_size=len(embeddings_tensor[0])
+        self.hidden_size=hidden_size
+        self.dropout_p=dropout
+        self.embedding=nn.Embedding.from_pretrained(embeddings_tensor)
+        self.gru=nn.GRU(
+            input_size=self.embedding_size,
+            hidden_size=self.hidden_size,
+            num_layers=3,
+            batch_first=True,
+            dropout=self.dropout_p,
+            bidirectional=False)
+        
+    def forward(self,input_tensor,hidden):
+        output=self.embedding(input_tensor)
+        output,hidden=self.gru(output,hidden)
+        return output,hidden
+    
+    def initHidden(self,batch_size):
+        n=1 if self.gru.bidirectional==False else 2
+        l=self.gru.num_layers
+        return torch.zeros(n*l, batch_size, self.hidden_size)
+
+class Decoder(nn.Module):
+    def __init__(self,
+                 embeddings_tensor,
+                 hidden_size=40,
+                 dropout=0.5):
+        super(Decoder, self).__init__()
+        self.embedding_size=len(embeddings_tensor[0])
+        self.hidden_size=hidden_size
+        self.dropout_p=dropout
+        
+        self.embedding=nn.Embedding.from_pretrained(embeddings_tensor)
+        self.gru=nn.GRU(
+            input_size=self.embedding_size,
+            hidden_size=self.hidden_size,
+            num_layers=3,
+            batch_first=True,
+            dropout=self.dropout_p,
+            bidirectional=False)
+        n=1 if self.gru.bidirectional==False else 2
+        self.out=nn.Linear(self.hidden_size*n, len(embeddings_tensor))
+
+    def forward(self,input_tensor,hidden):
+        output=self.embedding(input_tensor)
+        output=F.relu(output)
+        output,hidden=self.gru(output,hidden)
+        output=self.out(output)
+        return output,hidden
+    def predict(self, input_tensor, hidden):
+        output=self.embedding(input_tensor)
+        output=F.relu(output)
+        outputs = []
+        for loop:
+          output,hidden=self.gru(output,hidden)
+          output=self.out(output)
+          outputs.append()
+
+class SexPred(nn.Module):
+    """
+    Encoder-decoder model that imitates the behaviour of an online
+    sexual predator
+    """
+    def __init__(self,embeddings_tensor):
+        super(SexPred,self).__init__()
+        self.embeddings_tensor=embeddings_tensor
+        self.encoder=Encoder(self.embeddings_tensor)
+        self.decoder=Decoder(self.embeddings_tensor)
+    
+    def forward(self,vic_perv):
+        v=vic_perv[0]
+        p=vic_perv[1]
+        batch_size=len(v)
+        h_enc=self.encoder.initHidden(batch_size).to(device)
+        _,h=self.encoder(v,h_enc)
+        o,_=self.decoder(p,h)
+        return o
---
+++ src/train.py
@@ -0,0 +1,74 @@
+import torch,IPython
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm_notebook as tqdm
+
+def train(input_tensor,target,model,optimiser,criterion):
+    model.train()
+    optimiser.zero_grad()
+    prediction = model((input_tensor,target)).permute(0,2,1)
+    loss = criterion(prediction,target)
+    loss.backward()
+    # clip_grad_norm_(model.parameters(),clip)
+    optimiser.step()
+    return loss.item(), prediction
+
+def valid(input_tensor,target,model,criterion):
+    model.eval()
+    prediction = model((input_tensor,target)).permute(0,2,1)
+    loss = criterion(prediction,target)
+    return loss.item(), prediction
+
+def trainIters(model,
+               train_dset,
+               valid_dset,
+               batch_size,
+               n_epochs,
+               learning_rate,
+               weight_decay):
+    print("CUDA is available!" if torch.cuda.is_available() else "NO CUDA 4 U")
+
+    optimiser = torch.optim.Adam(model.parameters(), lr=learning_rate,weight_decay=weight_decay)
+    criterion = torch.nn.CrossEntropyLoss()
+    
+    train_dl=torch.utils.data.DataLoader(train_dset, batch_size=batch_size)
+    train_dl=tqdm(train_dl,desc='Training',leave=False)
+    
+    valid_dl=torch.utils.data.DataLoader(valid_dset, batch_size=batch_size)
+    valid_dl=tqdm(valid_dl,desc='Validating',leave=False)
+    
+    train_losses=[np.inf]
+    valid_losses=[np.inf]
+
+    for epoch in tqdm(range(1,n_epochs+1),desc='Epoch'):
+        plt.gca().cla()
+        plt.xlim(0,n_epochs)
+        plt.ylim(0,1)
+        plt.title("Learning curve")
+        plt.xlabel("Number of epochs")
+        plt.ylabel("Loss")
+        plt.text(n_epochs/2,.9,"Train loss: {:.2f}".format(train_losses[-1]))
+        plt.text(n_epochs/2,.8,"Validation loss: {:.2f}".format(valid_losses[-1]))
+        plt.plot(train_losses, "-b", label="Training loss")
+        plt.plot(valid_losses, "-r", label="Validation loss")
+        plt.legend(loc="upper left")
+        IPython.display.display(plt.gcf())
+        
+        
+        for x,y in train_dl:
+            input_tensor = x.to(device)
+            target = y.to(device)
+            train_loss, prediction = train(input_tensor,target,model,optimiser,criterion)
+        print("Training loss: {:.4f}".format(train_loss))
+        train_losses.append(train_loss)
+        
+        with torch.no_grad():
+            valid_loss = 0
+            for x, y in valid_dl:
+                input_tensor = x.to(device)
+                target = y.to(device)
+                v_loss, valid_pred = valid(input_tensor,target,model,criterion)
+            print("Validation loss: {:.4f}".format(v_loss))
+            valid_losses.append(v_loss)
+        IPython.display.clear_output(wait=True)
+    return train_losses,valid_losses
